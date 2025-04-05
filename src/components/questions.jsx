714e13c5208e0f31210fd669cc26e90a11fc5fd6
Vit Benton <Vit@vitbenton.com>
2025-04-05 09:56:35 -0400

    styling added for correct/incorrect answers

--- src/components/questions.jsx
+++ src/components/questions.jsx
@@ -9,9 +9,18 @@ const Questions = () => {
 
   // State
   const [successfulAnswers, setSuccessfulAnswers] = useState([]);
+  const [questionsAnswered, setQuestionsAnswered] = useState([]);
 
   // Methods
-  const handleSelect = useCallback((answer, correct_answer) => {
+  const getClassName = useCallback((correct_answer, question) => {
+    if (!questionsAnswered.includes(question)) return '';
+
+    return successfulAnswers.includes(correct_answer) ? 'correct' : 'incorrect';
+  });
+
+  const handleSelect = useCallback((answer, correct_answer, question) => {
+    setQuestionsAnswered(prevValues => [...prevValues, question])
+
     if (answer === correct_answer) {
       setSuccessfulAnswers(prevValues => [...prevValues, correct_answer])
     } else {
@@ -28,7 +37,7 @@ const Questions = () => {
       <h3>{`Correctly answered: ${successfulAnswers.length} out of ${questions.length}`}</h3>
       <ul id="questions-list">
         {questions.map(({ correct_answer, incorrect_answers, question }) => (
-          <li key={question}>
+          <li key={question} className={getClassName(correct_answer, question)}>
             <h4>{decodeHtml(question)}</h4>
 
             <fieldset>
@@ -40,7 +49,7 @@ const Questions = () => {
                   id={correct_answer}
                   name={`${question}-answer`}
                   value={correct_answer}
-                  onChange={event => handleSelect(event.target.value, correct_answer)}
+                  onChange={event => handleSelect(event.target.value, correct_answer, question)}
                 />
                 <label htmlFor={correct_answer}>{decodeHtml(correct_answer)}</label>
               </div>
@@ -52,7 +61,7 @@ const Questions = () => {
                     id={answer}
                     name={`${question}-answer`}
                     value={answer}
-                    onChange={event => handleSelect(event.target.value, correct_answer)}
+                    onChange={event => handleSelect(event.target.value, correct_answer, question)}
                   />
                   <label htmlFor={answer}>{decodeHtml(answer)}</label>
                 </div>
