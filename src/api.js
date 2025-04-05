33b4d4248238c188e85b06a6a849288dc6111b5d
Vit Benton <Vit@vitbenton.com>
2025-04-05 08:35:09 -0400

    minor refactor

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 const API_ENDPOINTS = {
   tokenFetch: 'https://opentdb.com/api_token.php?command=request',
-  tokenReset: 'https://opentdb.com/api_token.php',
+  tokenReset: 'https://opentdb.com/api_token.php?command=reset',
   getQuestions: 'https://opentdb.com/api.php',
   getGlobalCategoryMaximums: 'https://opentdb.com/api_count_global.php',
   getCategoryMaximums: 'https://opentdb.com/api_count.php',
@@ -8,7 +8,7 @@ const API_ENDPOINTS = {
 };
 
 export const resetToken = async (token = '') => {
-  const fetchEndpoint = `${API_ENDPOINTS.tokenReset}?command=reset&token=${token}`;
+  const fetchEndpoint = `${API_ENDPOINTS.tokenReset}&token=${token}`;
   const response = await fetch(fetchEndpoint);
   const data = await response.json();
 
